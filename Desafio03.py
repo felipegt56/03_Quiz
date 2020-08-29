ec5559b70c434cc6b8173c8b5d2459bd5ce724c2
João felipe <felipewgt56@gmail.com>
2020-08-28 22:20:20 -0300

    Criei o arquivo Desafio03.py

---
+++ Desafio03.py
@@ -0,0 +1,78 @@
+from time import sleep
+def len():
+  print('=='*40)
+len()
+print('                          QUESTIONÁRIO SOBRE PYTHON')
+len()
+score = 0
+print('''
+Q-1 O que e Python?
+a) Um conjunto de ferramentas de edição
+b) Um jogo
+c) Uma liguagem de programação
+''')
+questao1 = input().lower()
+if questao1 == 'c':
+  print('Resposta certa!')
+  score += 1
+else:
+  print('Resposta Incorreta!.')
+
+print('''
+Q-2 Qual a outra tradução para o nome python?
+a) Cobra
+b) Pipa
+c) código
+''')
+questao2 = input().lower()
+if questao2 == 'a':
+  print('Resposta certa!')
+  score += 1
+else:
+  print('Resposta Incorreta!.')
+
+print('''
+Q-3 Qual comando eu uso para importa módulos em Python?
+a) int
+b) import
+c) sqrt
+''')
+questao3 = input().lower()
+if questao3 == 'b':
+  print('Resposta certa!')
+  score += 1
+else:
+  print('Resposta Incorreta!.')
+
+print('''
+Q-4 Qual exemplo e um valor boleano?
+a) 2.0
+b) 5
+c TRUE / FALSE
+''')
+questao4 = input().lower()
+if questao4 == 'c':
+  print('Resposta certa!')
+  score += 1
+else:
+  print('Resposta Incorreta!.')
+
+print('''
+Q-5 Qual a função para imprima algo na tela?
+a) print
+b) float
+c)input
+''')
+questao5 = input().lower()
+if questao5 == 'a':
+  print('Resposta certa!')
+  score += 1
+else:
+  print('Resposta Incorreta!.')
+sleep(0.5)
+len()
+print('Calculando os pontos...')
+sleep(0.5)
+len()
+sleep(0.5)
+print(f'Sua pontuação foi de {score} pontos ')
